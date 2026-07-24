6b5907ebd0db927296cb48d1738428582f52f809
Kairav <kairavtupil@gmail.com>
2026-07-23 22:54:23 -0400

    completed baseline RAG with response generation

--- query.py
+++ query.py
@@ -1,5 +1,7 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
+from langchain_ollama import ChatOllama
+
 
 embeddings = OllamaEmbeddings(
     model="nomic-embed-text"
@@ -9,9 +11,19 @@ db = Chroma(
     embedding_function=embeddings,
     persist_directory="chroma_db"
 )
-
-chunks = db.similarity_search("How is multi-head attention used in transformers?",
+question = "How is multi-head attention used in transformers?"
+chunks = db.similarity_search(question,
                                 k=3)
 
+
+content = ''
 for page in chunks:
-    print(page.page_content)
+    content += f' {page.page_content}'
+
+prompt =\
+      f"Using the context {content} and NOTHING ELSE,\
+      answer the question \"{question}\"."
+
+chat = ChatOllama(model="llama3")
+context_answer = chat.invoke(prompt)
+print(context_answer.content)
